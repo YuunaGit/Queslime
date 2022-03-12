acdb0d0d79fdb062d3b6e70a426dcf7bdd26d863
Yuuna <comaAberration@gmail.com>
2022-03-12 10:12:34 +0800

    Update Info.java

--- src/main/java/com/queslime/enums/Info.java
+++ src/main/java/com/queslime/enums/Info.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 @Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum Info {
-    REGISTER_EMAIL_ILLEGAL(100, "非法邮箱格式"),
-    REGISTER_EMAIL_DUPLICATE(101, "邮箱已被使用"),
-    REGISTER_PWD_ILLEGAL(102,"密码长度应在6到20位之间，且至少包含一个字母和一个数字"),
-    REGISTER_SUCCESS(103, "注册账号成功"),
-    REGISTER_FAIL(104, "注册账号失败");
+    REGISTER_EMAIL_NULL(110, "邮箱不能为空"),
+    REGISTER_PWD_NULL(111, "密码不能为空"),
+    REGISTER_EMAIL_ILLEGAL(112, "非法邮箱格式"),
+    REGISTER_EMAIL_DUPLICATE(113, "邮箱已被使用"),
+    REGISTER_PWD_ILLEGAL(114,"密码长度应在6到20位之间，且至少包含一个字母和一个数字"),
+    REGISTER_SUCCESS(115, "注册账号成功"),
+    REGISTER_FAIL(116, "注册账号失败");
 
     private final int code;
     private final String msg;
