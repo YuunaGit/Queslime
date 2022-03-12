dd654fb5d454d6a74713be91f57f73a0c2a0d81c
Yuuna <comaAberration@gmail.com>
2022-03-12 13:27:19 +0800

    Update E_user.sql

--- src/main/resources/sql/E_user.sql
+++ src/main/resources/sql/E_user.sql
@@ -3,5 +3,6 @@ create table e_user(
     uid int auto_increment primary key,
     user_name tinytext not null,
     user_email tinytext not null,
-    user_password tinytext not null
+    user_password tinytext not null,
+    user_status tinyint not null
 );
