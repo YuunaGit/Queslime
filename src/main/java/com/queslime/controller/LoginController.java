2bd4446fa0d5fe82e4fb832db51df315259a4645
Yuuna <comaAberration@gmail.com>
2022-03-13 12:32:55 +0800

    Update LoginController.java

--- src/main/java/com/queslime/controller/LoginController.java
+++ src/main/java/com/queslime/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.queslime.controller;
 
+import com.queslime.entity.User;
+import com.queslime.enums.Info;
 import com.queslime.service.UserService;
+import com.queslime.utils.Encoder;
 import com.queslime.utils.Result;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +18,26 @@ public class LoginController {
     public Result loginUser(@RequestParam(value = "email", defaultValue = "")String userEmail,
                             @RequestParam(value = "password", defaultValue = "")String userPassword) {
         Result result = new Result();
-        return result;
+
+        if("".equals(userEmail)) {
+            return result.info(Info.LOGIN_EMAIL_NULL);
+        }
+
+        if("".equals(userPassword)) {
+            return result.info(Info.LOGIN_PWD_NULL);
+        }
+
+        User user = userService.selectOneByEmail(userEmail);
+
+        if(user == null) {
+            return result.info(Info.LOGIN_EMAIL_OR_PWD_WRONG);
+        }
+
+        if(Encoder.match(userPassword, user.getUserPassword())) {
+            return result.info(Info.LOGIN_SUCCESS);
+        }
+
+        return result.info(Info.LOGIN_EMAIL_OR_PWD_WRONG);
     }
 
 }
