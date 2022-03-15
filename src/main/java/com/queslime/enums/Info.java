d8d35df9dc42a358692aab4f091dfddf28d6014f
Yuuna <comaAberration@gmail.com>
2022-03-15 19:54:46 +0800

    Update Info.java

--- src/main/java/com/queslime/enums/Info.java
+++ src/main/java/com/queslime/enums/Info.java
@@ -17,15 +17,15 @@ public enum Info {
     REGISTER_EMAIL_ILLEGAL(111, "非法邮箱格式"),
     REGISTER_EMAIL_DUPLICATE(112, "邮箱已被使用"),
     REGISTER_PWD_ILLEGAL(113,"密码长度应在6到20位之间，且至少包含一个字母和一个数字"),
-    REGISTER_FAIL(114, "注册账号失败"),
+    REGISTER_FAIL(119, "注册账号失败"),
     // Activate
     ACTIVATE_SUCCESS(120, "账号激活成功"),
     ACTIVATE_ALREADY_ACTIVATED(121, "账号已经激活"),
     ACTIVATE_EMAIL_SEND(122, "邮件成功发送"),
-    ACTIVATE_FAIL(123, "错误代码"),
+    ACTIVATE_FAIL(129, "错误代码"),
     // Login
     LOGIN_SUCCESS(130, "登录成功"),
-    LOGIN_EMAIL_OR_PWD_WRONG(131, "邮箱或密码错误");
+    LOGIN_EMAIL_OR_PWD_WRONG(139, "邮箱或密码错误");
 
 
     private final int code;
