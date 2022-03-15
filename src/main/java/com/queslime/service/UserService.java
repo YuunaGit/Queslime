91bdf8c468abe27e80a438e0135e0387852745a9
Yuuna <comaAberration@gmail.com>
2022-03-15 10:02:02 +0800

    Update UserService.java

--- src/main/java/com/queslime/service/UserService.java
+++ src/main/java/com/queslime/service/UserService.java
@@ -6,6 +6,7 @@ import com.queslime.mapper.UserMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
@@ -22,14 +23,31 @@ public class UserService {
         return userMapper.selectById(uid);
     }
 
-    public User selectOneByEmail(String email) {
-        return userMapper.selectOne(
-                new QueryWrapper<User>().eq("user_email", email)
-        );
-    }
-
     // Update
     public int update(User user) {
         return userMapper.updateById(user);
+    }
+
+    // Service
+    public boolean isEmailIllegal(String email) {
+        Pattern emailRegex = Pattern.compile("^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$");
+        return !emailRegex.matcher(email).matches();
+    }
+
+    public boolean isEmailDuplicate(String email) {
+        return userMapper.selectOne(
+                new QueryWrapper<User>().eq("user_email", email)
+        ) != null;
+    }
+
+    public boolean isPasswordIllegal(String pwd) {
+        Pattern pwdRegex = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,20}$");
+        return !pwdRegex.matcher(pwd).matches();
+    }
+
+    public String generatedUserName() {
+        long time = System.currentTimeMillis();
+        long curr = 0x17F7B000000L;
+        return "User_" + Long.toHexString(time - curr);
     }
 }
