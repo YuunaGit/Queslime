1241330a2e3b53173530db63fdced727a41187a2
Yuuna <comaAberration@gmail.com>
2022-03-21 23:50:08 +0800

    Update ActivateController.java

--- src/main/java/com/queslime/controller/ActivateController.java
+++ src/main/java/com/queslime/controller/ActivateController.java
@@ -6,12 +6,14 @@ import com.queslime.enums.entityEnum.UserStatus;
 import com.queslime.service.UserService;
 import com.queslime.utils.EmailSender;
 import com.queslime.utils.Result;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@CrossOrigin
 @RestController
 public class ActivateController {
     @Resource
