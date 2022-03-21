dd52d56f8ec7542fbc2e7e1ec9b2f034a57dfdb4
Yuuna <comaAberration@gmail.com>
2022-03-21 23:18:24 +0800

    Update RegisterController.java

--- src/main/java/com/queslime/controller/RegisterController.java
+++ src/main/java/com/queslime/controller/RegisterController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
 
 @CrossOrigin
 @RestController
