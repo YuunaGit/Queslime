41a225cd4191ad93ee79ecb35c7bef97a6bbecc7
Yuuna <comaAberration@gmail.com>
2022-03-21 23:38:33 +0800

    Update RegisterController.java

--- src/main/java/com/queslime/controller/RegisterController.java
+++ src/main/java/com/queslime/controller/RegisterController.java
@@ -31,7 +31,7 @@ public class RegisterController {
             return result.info(Info.PWD_NULL);
         }
 
-        if(userEmail.length() >= 200) {
+        if(userEmail.length() >= 100) {
             return result.info(Info.REGISTER_EMAIL_TOO_LONG);
         }
 
@@ -57,9 +57,6 @@ public class RegisterController {
             return result.info(Info.FAIL);
         }
 
-        User user = userService.selectOneByEmail(userEmail);
-        var data = userService.userWrapper(user);
-
-        return result.info(Info.SUCCESS, data);
+        return result.info(Info.SUCCESS);
     }
 }
