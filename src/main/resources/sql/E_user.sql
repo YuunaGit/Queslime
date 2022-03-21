7d5dafbb361e0b386b3ff33a8e890cebded255d0
Yuuna <comaAberration@gmail.com>
2022-03-21 23:28:08 +0800

    Update E_user.sql

--- src/main/resources/sql/E_user.sql
+++ src/main/resources/sql/E_user.sql
@@ -1,8 +1,8 @@
 drop table if exists e_user;
 create table e_user(
     uid int auto_increment primary key,
-    user_name tinytext not null,
-    user_email varchar(200) unique not null,
+    user_name varchar(100) unique not null,
+    user_email varchar(100) unique not null,
     user_password tinytext not null,
     user_status tinyint not null,
     created_at timestamp(6) default current_timestamp(6) not null
