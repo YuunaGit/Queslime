ea0eeecfa6dfe18562b3178f7e00730585d46821
Yuuna <comaAberration@gmail.com>
2022-03-22 15:45:39 +0800

    Update Info.java

--- src/main/java/com/queslime/enums/Info.java
+++ src/main/java/com/queslime/enums/Info.java
@@ -14,19 +14,20 @@ public enum Info {
     EMAIL_NULL(111, "邮箱不能为空"),
     PWD_NULL(112, "密码不能为空"),
     USER_NAME_NULL(113, "用户名不能为空"),
+    // Public: Duplicate
+    EMAIL_DUPLICATE(120, "邮箱已被使用"),
+    USER_NAME_DUPLICATE(121, "用户名重复"),
     // Public: Success or Fail
     SUCCESS(200, "成功"),
     FAIL(201, "失败"),
     // Register
-    REGISTER_EMAIL_ILLEGAL(210, "非法邮箱格式"),
-    REGISTER_EMAIL_DUPLICATE(211, "邮箱已被使用"),
-    REGISTER_EMAIL_TOO_LONG(212, "邮箱长度过长"),
-    REGISTER_PWD_ILLEGAL(213,"密码长度应在6到20位之间，且至少包含一个字母和一个数字"),
+    EMAIL_ILLEGAL(210, "非法邮箱格式"),
+    EMAIL_TOO_LONG(211, "邮箱长度过长"),
+    PWD_ILLEGAL(212,"密码长度应在6到20位之间，且至少包含一个字母和一个数字"),
     // Activate
-    ACTIVATE_ALREADY_ACTIVATED(220, "账号已经激活过了"),
-    ACTIVATE_EMAIL_SEND(221, "邮件成功发送"),
+    ALREADY_ACTIVATED(220, "账号已经激活过了"),
+    EMAIL_SEND(221, "邮件成功发送");
     // Update
-    UPDATE_USER_NAME_DUPLICATE(231, "用户名重复");
 
     private final int code;
     private final String msg;
