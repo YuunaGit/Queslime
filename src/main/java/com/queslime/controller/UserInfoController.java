2ff01bb26c5c4e21593ed1096f1073148dd04e4f
Yuuna <comaAberration@gmail.com>
2022-03-22 15:26:47 +0800

    Update UserInfoController.java

--- src/main/java/com/queslime/controller/UserInfoController.java
+++ src/main/java/com/queslime/controller/UserInfoController.java
@@ -4,20 +4,22 @@ import com.queslime.entity.User;
 import com.queslime.enums.Info;
 import com.queslime.service.UserService;
 import com.queslime.utils.Result;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@CrossOrigin
 @RestController
 public class UserInfoController {
     @Resource
     private UserService userService;
 
     @RequestMapping("/update/user/name")
-    public Result updateUserProfile(@RequestParam(value = "uid", defaultValue = "")String uidString,
-                                    @RequestParam(value = "newName", defaultValue = "")String newUserName) {
+    public Result updateUserName(@RequestParam(value = "uid", defaultValue = "")String uidString,
+                                 @RequestParam(value = "newName", defaultValue = "")String newUserName) {
         Result result = new Result();
 
         int uid = userService.stringToUid(uidString);
