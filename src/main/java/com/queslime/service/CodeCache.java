5f72d90df67267560755710eb5c371b81da4624c
Yuuna <comaAberration@gmail.com>
2022-03-23 22:51:55 +0800

    Create CodeCache.java

---
+++ src/main/java/com/queslime/service/CodeCache.java
@@ -0,0 +1,30 @@
+package com.queslime.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Hashtable;
+
+@Service
+public class CodeCache {
+    private final Hashtable<Integer, String> cache = new Hashtable<>();
+
+    public int generateRandomCode() {
+        int code = (int) (System.currentTimeMillis() >> 32);
+        code ^= (int) System.currentTimeMillis();
+        return code < 0 ? -code : code;
+    }
+
+    public void putCode(Integer id, String code) {
+        cache.put(id, code);
+    }
+
+    public boolean validateCode(Integer id, String code) {
+        String cacheCode = cache.get(id);
+        if(cacheCode != null && cacheCode.equals(code)) {
+            cache.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+}
