f5ac64de9b7131c761385da2a752465cc5093429
Yuuna <comaAberration@gmail.com>
2022-04-01 18:35:23 +0800

    Update E_user.sql

--- src/main/resources/sql/E_user.sql
+++ src/main/resources/sql/E_user.sql
@@ -4,6 +4,6 @@ create table e_user(
     user_name varchar(100) unique not null,
     user_email varchar(100) unique not null,
     user_password tinytext not null,
-    user_status tinyint not null,
+    user_state tinyint not null,
     created_at timestamp(6) default current_timestamp(6) not null
 );
