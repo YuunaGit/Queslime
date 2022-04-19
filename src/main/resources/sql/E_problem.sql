6184dda79481d7e38ac21410475875fc424ca71d
Yuuna <comaAberration@gmail.com>
2022-04-19 21:47:16 +0800

    Update E_problem.sql

--- src/main/resources/sql/E_problem.sql
+++ src/main/resources/sql/E_problem.sql
@@ -1,6 +1,6 @@
 drop table if exists e_problem;
 create table e_problem(
    pid int auto_increment primary key,
-   problem_title tinytext not null,
+   uid int not null,
    problem_content text not null
 );
