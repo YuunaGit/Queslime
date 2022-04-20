4547873d19adfd301fd6e915cb87fb229d05b9bf
Yuuna <comaAberration@gmail.com>
2022-04-20 20:42:58 +0800

    Create Problem.java

---
+++ src/main/java/com/queslime/entity/Problem.java
@@ -0,0 +1,28 @@
+package com.queslime.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@TableName(schema = "queslime", value = "e_problem")
+public class Problem {
+    @TableId(type = IdType.AUTO)
+    private Integer pid;
+    private Integer uid;
+    private String problemContent;
+
+    public Problem(Integer pid, Integer uid, String problemContext) {
+        this.pid = pid;
+        this.uid = uid;
+        this.problemContent = problemContext;
+    }
+}
+
