55eeaafe305e83dcbf7b3048d92961d81182c0e1
Yuuna <comaAberration@gmail.com>
2022-04-21 11:54:37 +0800

    Create ProblemService.java

---
+++ src/main/java/com/queslime/service/ProblemService.java
@@ -0,0 +1,12 @@
+package com.queslime.service;
+
+import com.queslime.mapper.ProblemMapper;
+
+import javax.annotation.Resource;
+
+public class ProblemService {
+    @Resource
+    private ProblemMapper problemMapper;
+
+
+}
