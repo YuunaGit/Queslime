9d41f05875110db52236d12a169adfc77f8cf4ff
Yuuna <comaAberration@gmail.com>
2022-04-21 11:54:34 +0800

    Create ProblemMapper.java

---
+++ src/main/java/com/queslime/mapper/ProblemMapper.java
@@ -0,0 +1,8 @@
+package com.queslime.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.queslime.entity.Problem;
+
+public interface ProblemMapper extends BaseMapper<Problem> {
+
+}
