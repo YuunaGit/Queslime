41a69b1c5eba7074d0681f0644dfec281c58b50f
Yuuna <comaAberration@gmail.com>
2022-05-04 23:17:50 +0800

    Update ProblemController.java

--- src/main/java/com/queslime/controller/ProblemController.java
+++ src/main/java/com/queslime/controller/ProblemController.java
@@ -1,16 +1,16 @@
 package com.queslime.controller;
 
+import com.queslime.entity.Problem;
+import com.queslime.entity.ProblemWithTags;
 import com.queslime.entity.User;
 import com.queslime.enums.Info;
 import com.queslime.service.ProblemService;
+import com.queslime.service.ProblemWithTagsService;
 import com.queslime.service.UserService;
 import com.queslime.utils.Result;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 @CrossOrigin
 @RestController
@@ -21,10 +21,13 @@ public class ProblemController {
     @Resource
     private ProblemService problemService;
 
+    @Resource
+    private ProblemWithTagsService problemWithTagsService;
+
     @RequestMapping(value = "/post/problem")
     public Result postProblem(@RequestParam(value = "uid", defaultValue = "")String uidString,
                               @RequestParam(value = "content", defaultValue = "")String content,
-                              @RequestParam(value = "tags", defaultValue = "")String[] tags) {
+                              @RequestParam(value = "tags", defaultValue = "")String[] tagsString) {
         Result result = new Result();
 
         if("".equals(uidString)) {
@@ -35,7 +38,7 @@ public class ProblemController {
             return result.info(Info.QUESTION_NULL);
         }
 
-        if(tags.length == 0) {
+        if(tagsString.length == 0) {
             return result.info(Info.TAGS_NULL);
         }
 
@@ -49,9 +52,34 @@ public class ProblemController {
             return result.info(Info.UID_NOT_EXISTS);
         }
 
+        Problem newProblem = new Problem(
+            user.getUid(),
+            content
+        );
 
+        if(problemService.insert(newProblem) == 0) {
+            return result.info(Info.FAIL);
+        }
 
+        int pid = problemService.selectCount();
 
-        return result;
+        int tagsCount = tagsString.length;
+        int[] tags = new int[tagsCount];
+        for(int i = 0; i < tagsCount; i++) {
+            try {
+                tags[i] = Integer.parseInt(tagsString[i]);
+            } catch (NumberFormatException e) {
+                return result.info(Info.PROBLEM_TAG_ILLEGAL);
+            }
+        }
+
+        for(int tid : tags) {
+            ProblemWithTags pwt = new ProblemWithTags(pid, tid);
+            if(problemWithTagsService.insert(pwt) == 0) {
+                return result.info(Info.FAIL);
+            }
+        }
+
+        return result.info(Info.SUCCESS);
     }
 }
