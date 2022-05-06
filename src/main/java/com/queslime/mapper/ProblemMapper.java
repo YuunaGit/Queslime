8293ddec6ba50d57c4d9ba712571552d59abb3c0
Yuuna <comaAberration@gmail.com>
2022-05-06 11:40:48 +0800

    Update ProblemMapper.java

--- src/main/java/com/queslime/mapper/ProblemMapper.java
+++ src/main/java/com/queslime/mapper/ProblemMapper.java
@@ -2,7 +2,9 @@ package com.queslime.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.queslime.entity.Problem;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ProblemMapper extends BaseMapper<Problem> {
 
 }
