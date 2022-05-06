c886f9fc55bb668c9c44945c7092932c6f7e0ce7
Yuuna <comaAberration@gmail.com>
2022-05-06 11:40:50 +0800

    Create ProblemWithTagsMapper.java

---
+++ src/main/java/com/queslime/mapper/ProblemWithTagsMapper.java
@@ -0,0 +1,10 @@
+package com.queslime.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.queslime.entity.ProblemWithTags;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProblemWithTagsMapper extends BaseMapper<ProblemWithTags> {
+
+}
