2f997189dfb22dbb573d19ed373487c9b7ac98bf
Yuuna <comaAberration@gmail.com>
2022-05-07 18:04:42 +0800

    Create Solution.java

---
+++ src/main/java/com/queslime/entity/Solution.java
@@ -0,0 +1,31 @@
+package com.queslime.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@TableName(schema = "queslime", value = "e_solution")
+public class Solution {
+    @TableId(type = IdType.AUTO)
+    private Integer sid;
+    private Integer pid;
+    private Integer uid;
+    private String solutionContent;
+    private Timestamp createdAt;
+
+    public Solution(Integer pid, Integer uid, String solutionContent) {
+        this.pid = pid;
+        this.uid = uid;
+        this.solutionContent = solutionContent;
+    }
+}
