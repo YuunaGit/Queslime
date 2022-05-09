6b6d5cce36088cf9afd5abc75bd801be9d5ea028
Yuuna233 <101407816+YuunaGit@users.noreply.github.com>
2022-05-09 15:21:43 +0800

    Update SolutionService.java

--- src/main/java/com/queslime/service/SolutionService.java
+++ src/main/java/com/queslime/service/SolutionService.java
@@ -1,5 +1,6 @@
 package com.queslime.service;
 
+import com.queslime.entity.Solution;
 import com.queslime.mapper.SolutionMapper;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,18 @@ public class SolutionService {
     @Resource
     private SolutionMapper solutionMapper;
 
+    // Create
+    public int insert(Solution solution) {
+        return solutionMapper.insert(solution);
+    }
 
+    // Update
+    public int update(Solution solution) {
+        return solutionMapper.updateById(solution);
+    }
+
+    // Retrieve
+    public Solution selectOneBySid(int sid) {
+        return solutionMapper.selectById(sid);
+    }
 }
