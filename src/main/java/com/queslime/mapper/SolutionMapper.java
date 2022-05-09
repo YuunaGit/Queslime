b8b0b77d4edc8581ff8e77ae43449611c1d91c97
Yuuna <comaAberration@gmail.com>
2022-05-09 12:45:40 +0800

    Create SolutionMapper.java

---
+++ src/main/java/com/queslime/mapper/SolutionMapper.java
@@ -0,0 +1,10 @@
+package com.queslime.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.queslime.entity.Solution;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SolutionMapper extends BaseMapper<Solution> {
+
+}
