166042db616f3df04fd41987b84b568fa15d5e6f
Yuuna <comaAberration@gmail.com>
2022-05-09 12:45:57 +0800

    Create E_solution.sql

---
+++ src/main/resources/sql/E_solution.sql
@@ -0,0 +1,8 @@
+drop table if exists e_solution;
+create table e_solution(
+    sid int auto_increment primary key,
+    pid int not null,
+    uid int not null,
+    solution_content text not null,
+    created_at timestamp default current_timestamp not null
+);
