ab61779a56bfa7f40389b6373022117f42d5be97
Yuuna233 <101407816+YuunaGit@users.noreply.github.com>
2022-05-11 19:56:36 +0800

    Create ReplyMapper.java

---
+++ src/main/java/com/queslime/mapper/ReplyMapper.java
@@ -0,0 +1,11 @@
+package com.queslime.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.queslime.entity.Reply;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ReplyMapper  extends BaseMapper<Reply>{
+    
+}
