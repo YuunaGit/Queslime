93765e3bd3a958fe30a2ca1ba09f57bd60d576b2
Yuuna <comaAberration@gmail.com>
2022-05-12 19:26:32 +0800

    Update Problem.java

--- src/main/java/com/queslime/entity/Problem.java
+++ src/main/java/com/queslime/entity/Problem.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -20,10 +21,31 @@ public class Problem {
     private Integer pid;
     private Integer uid;
     private String problemContent;
+    private Integer difficulty;
     private Timestamp createdAt;
+    private Integer bestSid;
+    private Integer viewCount;
+    private Integer solutionCount;
 
-    public Problem(Integer uid, String problemContent) {
+    public Problem(Integer uid, String problemContent, Integer difficulty) {
         this.uid = uid;
         this.problemContent = problemContent;
+        this.difficulty = difficulty;
+        this.bestSid = 0;
+        this.viewCount = 0;
+        this.solutionCount = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Problem problem = (Problem) o;
+        return pid.equals(problem.pid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pid);
     }
 }
