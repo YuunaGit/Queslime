dd4fd187e76073d673291d040799113054137ea6
Yuuna <comaAberration@gmail.com>
2022-05-13 12:21:47 +0800

    Update ReplyService.java

--- src/main/java/com/queslime/service/ReplyService.java
+++ src/main/java/com/queslime/service/ReplyService.java
@@ -1,5 +1,10 @@
 package com.queslime.service;
 
+import com.queslime.mapper.ReplyMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
 @Service
 public class ReplyService {
     @Resource
