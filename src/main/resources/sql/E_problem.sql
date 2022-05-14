69b757d49a0f2d1455e959f99b07e943a67f501e
Yuuna <comaAberration@gmail.com>
2022-05-14 12:08:30 +0800

    Update E_problem.sql

--- src/main/resources/sql/E_problem.sql
+++ src/main/resources/sql/E_problem.sql
@@ -4,5 +4,8 @@ create table e_problem(
    uid int not null,
    problem_content text not null,
    difficulty tinyint not null,
-   created_at timestamp default current_timestamp not null
+   created_at timestamp default current_timestamp not null,
+   best_sid int not null,
+   view_count int not null,
+   solution_count int not null
 );
