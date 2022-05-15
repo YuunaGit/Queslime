19c09a15a0702df00cffd60747768e889400b6f7
Yuuna <comaAberration@gmail.com>
2022-05-15 18:12:57 +0800

    Update TagsController.java

--- src/main/java/com/queslime/controller/TagsController.java
+++ src/main/java/com/queslime/controller/TagsController.java
@@ -29,22 +29,22 @@ public class TagsController {
         return result.info(Info.SUCCESS);
     }
 
-    @RequestMapping(value = "/get/tags")
-    public Result getTagsByType(@RequestParam(value = "type", defaultValue = "")String typeIdString) {
-        Result result = new Result();
-
-        int typeId;
-        try{
-            typeId = Integer.parseInt(typeIdString);
-        } catch (NumberFormatException e) {
-            return result.info(Info.PROBLEM_TAG_ILLEGAL);
-        }
-
-        var tagsList = tagService.selectTagsByType(typeId);
-        if(tagsList == null) {
-            return result.info(Info.FAIL);
-        }
-
-        return result.info(Info.SUCCESS, tagsList);
-    }
+//    @RequestMapping(value = "/get/tags")
+//    public Result getTagsByType(@RequestParam(value = "type", defaultValue = "")String typeIdString) {
+//        Result result = new Result();
+//
+//        int typeId;
+//        try{
+//            typeId = Integer.parseInt(typeIdString);
+//        } catch (NumberFormatException e) {
+//            return result.info(Info.PROBLEM_TAG_ILLEGAL);
+//        }
+//
+//        var tagsList = tagService.selectTagsByType(typeId);
+//        if(tagsList == null) {
+//            return result.info(Info.FAIL);
+//        }
+//
+//        return result.info(Info.SUCCESS, tagsList);
+//    }
 }
