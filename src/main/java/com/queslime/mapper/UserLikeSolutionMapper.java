bc7c6f96481d0e513ad8f274fc4b080f217a2aeb
Yuuna <comaAberration@gmail.com>
2022-05-16 19:01:33 +0800

    Create UserLikeSolutionMapper.java

---
+++ src/main/java/com/queslime/mapper/UserLikeSolutionMapper.java
@@ -0,0 +1,10 @@
+package com.queslime.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.queslime.entity.UserLikeSolution;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserLikeSolutionMapper extends BaseMapper<UserLikeSolution> {
+
+}
