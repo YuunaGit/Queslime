f9f34d5c58a464909f637ebcab4f6c3ded05ea42
Yuuna <comaAberration@gmail.com>
2022-05-16 19:01:40 +0800

    Update ReplyService.java

--- src/main/java/com/queslime/service/ReplyService.java
+++ src/main/java/com/queslime/service/ReplyService.java
@@ -1,5 +1,6 @@
 package com.queslime.service;
 
+import com.queslime.entity.Reply;
 import com.queslime.mapper.ReplyMapper;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,20 @@ public class ReplyService {
     @Resource
     private ReplyMapper replyMapper;
 
-    
+    // Create
+    public int insert(Reply reply) {
+        return replyMapper.insert(reply);
+    }
+
+    // Update
+    public int update(Reply reply) {
+        return replyMapper.updateById(reply);
+    }
+
+    // Retrieve
+    public Reply selectOneBySid(long rid) {
+        return replyMapper.selectById(rid);
+    }
+
+
 }
