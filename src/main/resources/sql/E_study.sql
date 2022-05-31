7f6bc2ae2f6f4cdb1ccd5aa3138225cd30edd031
Yuuna <comaAberration@gmail.com>
2022-05-31 15:54:42 +0800

    Update E_study.sql

--- src/main/resources/sql/E_study.sql
+++ src/main/resources/sql/E_study.sql
@@ -2,5 +2,7 @@ drop table if exists e_study;
 create table e_study(
     uid int not null,
     pid int not null,
-    pass tinyint not null
+    pass tinyint not null,
+    difficulty tinyint not null,
+    created_at timestamp default current_timestamp not null
 );
