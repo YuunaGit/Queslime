a1ec6689517f4a179a23ef83240a466f1a7eb96f
Yuuna <comaAberration@gmail.com>
2022-05-31 15:54:37 +0800

    Update ReplyService.java

--- src/main/java/com/queslime/service/ReplyService.java
+++ src/main/java/com/queslime/service/ReplyService.java
@@ -2,16 +2,22 @@ package com.queslime.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.queslime.entity.Reply;
+import com.queslime.entity.Solution;
+import com.queslime.entity.User;
 import com.queslime.mapper.ReplyMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 public class ReplyService {
     @Resource
     private ReplyMapper replyMapper;
+
+    @Resource
+    private UserService userService;
 
     // Create
     public int insert(Reply reply) {
@@ -34,5 +40,17 @@ public class ReplyService {
         );
     }
 
+    public HashMap<String, Object> replyWrapper(Reply reply) {
+        var data = new HashMap<String, Object>();
 
+        data.put("rid", reply.getRid());
+        data.put("sid", reply.getSid());
+        User user  = userService.selectOneByUid(reply.getPostUid());
+        data.put("post_uid", user.getUid());
+        data.put("user_name", user.getUserName());
+        data.put("content", reply.getReplyContent());
+        data.put("created_at", reply.getCreatedAt());
+
+        return data;
+    }
 }
