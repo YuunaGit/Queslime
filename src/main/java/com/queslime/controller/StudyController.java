174796c80957c191ed80ad56898a5beb5b910483
Yuuna <comaAberration@gmail.com>
2022-05-31 21:18:17 +0800

    Update StudyController.java

--- src/main/java/com/queslime/controller/StudyController.java
+++ src/main/java/com/queslime/controller/StudyController.java
@@ -35,9 +35,9 @@ public class StudyController {
     private ProblemWithTagsService problemWithTagsService;
 
     @RequestMapping(value = "/study")
-    public Result getNextProblem(@RequestParam(value = "uid", defaultValue = "")String uidString,
-                                 @RequestParam(value = "pid", defaultValue = "")String pidString,
-                                 @RequestParam(value = "pass", defaultValue = "") String passString) {
+    public Result getStudyData(@RequestParam(value = "uid", defaultValue = "")String uidString,
+                               @RequestParam(value = "pid", defaultValue = "")String pidString,
+                               @RequestParam(value = "pass", defaultValue = "") String passString) {
         Result result = new Result();
 
         if ("".equals(pidString)) {
@@ -85,9 +85,7 @@ public class StudyController {
 
         var newStudy = new Study(user.getUid(), problem.getPid(), pass, problem.getDifficulty());
 
-        if (studyService.insert(newStudy) == 0) {
-            return result.info(Info.FAIL);
-        }
+        studyService.insert(newStudy);
         
         return result.info(Info.SUCCESS);
     }
@@ -178,21 +176,24 @@ public class StudyController {
         var passList = (ArrayList<Study>) studyService.selectPassListByUid(uid);
         var notPassList = (ArrayList<Study>) studyService.selectNotPassListByUid(uid);
 
-        var diff0passNum = studyService.selectCountDiffByUid(uid, 1, 0);
-        var diff0NotPassNum = studyService.selectCountDiffByUid(uid, 0, 0);
-        var diff0PassNotData = new HashMap<String, Long>();
+        var diff0PassAndNotData = studyService.selectPassAndNotPassCountByUidDiff(uid, 0);
+        var diff0passNum = diff0PassAndNotData[0];
+        var diff0NotPassNum = diff0PassAndNotData[1];
+        var diff0PassNotData = new HashMap<String, Integer>();
         diff0PassNotData.put("pass_num", diff0passNum);
         diff0PassNotData.put("not_pass_num", diff0NotPassNum);
 
-        var diff1passNum = studyService.selectCountDiffByUid(uid, 1, 1);
-        var diff1NotPassNum = studyService.selectCountDiffByUid(uid, 0, 1);
-        var diff1PassNotData = new HashMap<String, Long>();
+        var diff1PassAndNotData = studyService.selectPassAndNotPassCountByUidDiff(uid, 1);
+        var diff1passNum = diff1PassAndNotData[0];
+        var diff1NotPassNum = diff1PassAndNotData[1];
+        var diff1PassNotData = new HashMap<String, Integer>();
         diff1PassNotData.put("pass_num", diff1passNum);
         diff1PassNotData.put("not_pass_num", diff1NotPassNum);
 
-        var diff2passNum = studyService.selectCountDiffByUid(uid, 1, 2);
-        var diff2NotPassNum = studyService.selectCountDiffByUid(uid, 0, 2);
-        var diff2PassNotData = new HashMap<String, Long>();
+        var diff2PassAndNotData = studyService.selectPassAndNotPassCountByUidDiff(uid, 2);
+        var diff2passNum = diff2PassAndNotData[0];
+        var diff2NotPassNum = diff2PassAndNotData[1];
+        var diff2PassNotData = new HashMap<String, Integer>();
         diff2PassNotData.put("pass_num", diff2passNum);
         diff2PassNotData.put("not_pass_num", diff2NotPassNum);
 
